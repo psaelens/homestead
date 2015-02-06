362232272026ab573933ae12153e0cfa38505fbd
Pierre Saelens <psaelens@gmail.com>
2015-02-06 12:27:46 +0100

    added git clone of neteo projects

--- init.sh
+++ init.sh
@@ -7,3 +7,14 @@ cp src/stubs/after.sh ~/.homestead/after.sh
 cp src/stubs/aliases ~/.homestead/aliases
 
 echo "Homestead initialized!"
+
+mkdir -p ~/Projects/Neteo
+
+git clone git@bitbucket.org:psaelens/neteo-core.git
+git clone git@bitbucket.org:psaelens/neteo-webapp.git
+
+echo "Neteo initialized!"
+echo "Update your /etc/hosts with: "
+echo "# Host Neteo "
+echo "192.168.10.10	neteo-webapp.app"
+echo "192.168.10.10	neteo-core.app"
