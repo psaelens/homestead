63ed37b5094a13b4af17edb1e22afba135cadbe5
Pierre Saelens <psaelens@gmail.com>
2016-02-05 15:59:36 +0100

    fixed nmap script provisionning

--- nmap-config.sh
+++ nmap-config.sh
@@ -4,5 +4,6 @@ cd /home/vagrant/neteo-core/
 
 gcc run_nmap.c
 sudo mv a.out /usr/bin/scanner
+sudo chown root:root /usr/bin/scanner
 sudo chmod a+s /usr/bin/scanner
 sudo chmod a+x /usr/bin/scanner
