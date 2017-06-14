82deca82a459e6a04a88097d9c93f455e61b8f55
Pierre Saelens <psaelens@gmail.com>
2017-06-14 10:12:37 +0200

    updated scripts

--- supervisor-config.sh
+++ supervisor-config.sh
@@ -10,19 +10,19 @@ autostart=true
 autorestart=true
 "
 
-echo "$block" > "/etc/supervisor/conf.d/neteo-queue.conf"
+#echo "$block" > "/etc/supervisor/conf.d/neteo-queue.conf"
 
-supervisorctl reread  
-supervisorctl add neteo-queue
-supervisorctl start neteo-queue   
+#supervisorctl reread  
+#supervisorctl add neteo-queue
+#supervisorctl start neteo-queue   
 
 # Update config to access supervisord configuration trought RPC 
-block2="[inet_http_server]
-port = *:9001
-username = user
-password = 123
+block2="[inet_http_server] \\
+port = *:9001 \\
+username = user \\
+password = 123 \\
 "
 
 sed -i "7i${block2}" /etc/supervisor/supervisord.conf
 
-service supervisor restart
+#service supervisor restart
